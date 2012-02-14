4a1fc15aa0a046f83edfa48550962606f74a4e1d
Kevin Pause <kevin.pause@supinfo.com>
2012-02-14 13:09:23 +0100

    Ajout de index.php et du dossier application

---
+++ application/templates/header.tpl
@@ -0,0 +1,1 @@
+	<img src="<?php echo $this->images_folder. 'logo.png' ?>"/><?php echo $this->GetLoginForm() ?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+/* AUTHOR : Kevin PAUSE
+ * UPDATE : Febrary, 2nd 2012
+ */
+ 
+include_once('application/controller.php');
+include_once('application/functions.php');
+include_once('application/forms.php');
+include_once('application/library/layout.php');
+
+register_shutdown_function(array('Controller', 'Initialize'));
+?>
